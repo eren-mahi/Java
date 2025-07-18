c79014425067f77583a5edcdb9626f9fc83a3166
Mahendra Udayakumar <150884494+eren-mahi@users.noreply.github.com>
2025-07-18 23:31:46 +0530

    Create Password_Strength.java

---
+++ Password_Strength.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner pass = new Scanner(System.in);
+        System.out.println("Enter Password");
+        String pd = pass.nextLine();
+        if(pd.length()>7)
+        {
+            int sm = 0;
+            int up = 0;
+            int dg = 0;
+            int sp = 0;
+            for(int i=0;i<pd.length();i++)
+            {
+                char ch = pd.charAt(i);
+                if(Character.isUpperCase(ch))
+                up++;
+                else if(Character.isLowerCase(ch))
+                sm++;
+                else if(Character.isDigit(ch))
+                dg++;
+                else
+                sp++;
+            }
+            if(sm>0 && up>0 && dg>0 && sp>0)
+            System.out.println("Good Password");
+            else
+            System.out.println("Bad Password");
+        }
+        else
+        System.out.println("Insuffient Length");
+       
+    }
+}
